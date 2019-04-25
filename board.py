aeebea32530df1f5128f8268d7739d1628d1bec0
Angel ILIEV <a.v.iliev13@gmail.com>
2019-04-25 15:51:06 +0300

    - Refactored simple tttoe board:   * Doesn't return result value WIN/LOSS/DRAW but instead returns who is the winner. This is used for the main (ultimate) board to determine winner there.   * Removed keeping track of whose side to move it is (also no playerJustMoved). This is now kept in the main board.   * Removed history from individual boards. Now this is kept in main board   * Added method to return a list of tttoe rows (string representation) which are used to build a console output for the full main board - Added gui file to introduce pygame GUI elements to the game - Implemented Ultimate board functionality:   * Getting all moves & getting available moves   * Take move/Make move   * Board copy   * Board string representation   * Get result (Single board that is a draw is considered unavailable for both players) - Added .png from pygame example to help me build understanding for pygame

--- board.py
+++ board.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 PLAYER_X = 1
 PLAYER_O = -1
 NO_PLAYER = 0
@@ -20,9 +17,9 @@ WIN = 1.0
 
 
 class BaseBoard:
-    """Defines the general structure which a board implementation must implement"""
-    def __init__(self):
-        raise NotImplementedError
+    """Defines the general structure which a board implementation
+    must implement
+    """
 
     def __str__(self):
         raise NotImplementedError
@@ -46,58 +43,47 @@ class BaseBoard:
 class Board:
     def __init__(self):
         self.pos = [0] * BOARD_SIZE
-        self.side = PLAYER_X
-        self.playerJustMoved = PLAYER_O
-        self.history = deque()
 
-    def __str__(self):
+    def get_row_strings(self):
         lines = []
         for combo in zip(*[self.pos[i::ROWS] for i in range(ROWS)]):
-            lines.extend(['{:<5}'.format(STR_MATRIX[elem]) for elem in combo])
-            lines.append('\n')
-        return ''.join(lines)
+            lines.append(''.join(['{:<5}'.format(STR_MATRIX[elem]) for elem in combo]))
+        return lines
 
     def __copy__(self):
         _b = Board()
         _b.pos = self.pos[:]  # copy list
-        _b.side = self.side  # todo remove this, not needed since player just moved
-        _b.playerJustMoved = self.playerJustMoved
-        _b.history = self.history.copy()  # todo copying deque is too slow
         return _b
 
-    def make_move(self, move):
+    def make_move(self, move, side):
         assert move in self.get_moves(), 'Position is already occupied'
 
-        self.pos[move] = self.side
-        self.side = -self.side  # change side to move
-        self.playerJustMoved = -self.playerJustMoved
-        self.history.append(move)
+        self.pos[move] = side
 
-    def take_move(self):
-        move = self.history.pop()
+    def take_move(self, move):
         self.pos[move] = NO_PLAYER
-        self.side = -self.side  # change side to move
-        self.playerJustMoved = -self.playerJustMoved
 
     def get_moves(self):
         return [idx for idx, value in enumerate(self.pos) if value == NO_PLAYER]
 
-    def get_result(self, player_jm):
-        cols_combo = [self.pos[i::ROWS] for i in range(ROWS)]
+    def get_result(self, board=None):
+        if board is None:
+            board = self.pos
+
+        cols_combo = [board[i::ROWS] for i in range(ROWS)]
         rows_combo = list(zip(*cols_combo))
-        # print(cols_combo)
-        # print(row s_combo)
 
         for i in range(ROWS):
             # Sum a row and a column
             row_result, col_result = sum(rows_combo[i]), sum(cols_combo[i])
 
-            # Check if sum of values of a row is not equal to number of rows i.e. all 1s or all -1s
+            # Check if sum of values of a row is not equal to number of rows
+            # i.e. all 1s or all -1s
             if abs(row_result) == ROWS:
-                return WIN if int(row_result / ROWS) == player_jm else LOSS
+                return int(row_result / ROWS)
 
             if abs(col_result) == ROWS:
-                return WIN if int(col_result / ROWS) == player_jm else LOSS
+                return int(col_result / ROWS)
 
         # Sum values on Right diagonal
         # Look at right Diagonal
@@ -105,18 +91,21 @@ class Board:
         # i.e. if you have [1, 2, 3,
         #                   4, 5, 6,
         #                   7 ,8 ,9] then right diagonal is [3, 5, 7]
-        # i.e. starting from the right corner the diagonal is formed by every second number
-        # (3, 5, 7), however this will also result in 9 being included which it should not be
-        # therefore we remove it
-        result = sum(self.pos[ROWS - 1::ROWS - 1][:-1])
+        # i.e. starting from the right corner the diagonal is formed by
+        # every second number (3, 5, 7), however this will also result
+        # in 9 being included which it should not be therefore we remove it
+        result = sum(board[ROWS - 1::ROWS - 1][:-1])
         if abs(result) == ROWS:
-            return WIN if int(result / ROWS) == player_jm else LOSS
+            return int(result / ROWS)
 
         # Left diagonal
-        result = sum(self.pos[::ROWS + 1])
+        result = sum(board[::ROWS + 1])
         if abs(result) == ROWS:
-            return WIN if int(result / ROWS) == player_jm else LOSS
+            return int(result / ROWS)
 
         # Lastly check if no available squares are on the board => TIE
-        if sum([abs(elem) for elem in self.pos]) == BOARD_SIZE:
+        if sum([abs(elem) for elem in board]) == BOARD_SIZE:
+            # here 0.5 indicates a DRAW and for ultimate tttoe
+            # this means that a drawn board is not taken into account for
+            # any player
             return DRAW
--- ultimate_board.py
+++ ultimate_board.py
@@ -1,24 +1,163 @@
-from ultimate_tttoe.board import *
+import random
+from collections import deque, namedtuple
+from pprint import pprint
+from typing import List, Tuple
+
+from board import *
+
+
+Move = namedtuple('Move', ['board_idx', 'move_idx'])
 
 
 class UltimateBoard(BaseBoard):
     def __init__(self):
-        pass
+        self.pos = [Board() for _ in range(BOARD_SIZE)]
+        # indicates where (which board) the next move should be done
+        self.nextBoard = None
+        self.playerJustMoved = PLAYER_O
+        self.history = deque()
 
     def __str__(self):
-        pass
+        lines = []
+        for combo in zip(*[self.pos[i::ROWS] for i in range(ROWS)]):
+            rows = zip(*[elem.get_row_strings() for elem in combo])
+            row = [''.join(['\t|\t'.join(row), '\n']) for row in rows]
+            row_length = sum([len(r) for r in row]) // ROWS
+            lines.extend(row)
+            lines.append(''.join(['_'*row_length, '\n']))
+        return ''.join(lines)
 
     def __copy__(self):
-        pass
+        _b = UltimateBoard()
+        _b.pos = [board.__copy__() for board in self.pos]
+        _b.nextBoard = self.nextBoard
+        _b.playerJustMoved = self.playerJustMoved
+        _b.history = self.history.copy()
+        return _b
 
-    def make_move(self, move):
-        pass
+    def make_move(self, move: int, board: int = None):
+        """Make a move to the specified board, if no board is specified
+        the move is done on the board indicated by nextBoard i.e forced
+        by the last played move.
+
+        @param move: move to play (i.e. index on the board)
+        @param board: index indicating which board to play the move on
+        """
+        self.playerJustMoved = -self.playerJustMoved
+
+        if self.nextBoard is None and board is None:
+            print('Need to provide a board idx at start of game')
+            return
+
+        if self.nextBoard is not None:
+            # if we are forced to play on a board (due to opponents move)
+            # check if nextBoard has a winner. If it does => player should
+            # have picked a new board to play on
+            if self.pos[self.nextBoard].get_result() is not None:
+                if board is None:
+                    print('The forced board already has a result on it.'
+                          'Please choose a different board.')
+                    return
+                else:
+                    if self.pos[board].get_result() is not None:
+                        print('The chosen board has a result on it.'
+                              'Please chose a different board.')
+                        return
+
+                    self.pos[board].make_move(move, self.playerJustMoved)
+                    self.history.append(Move(board_idx=board, move_idx=move))
+                    self.nextBoard = move  # the move on the board represents the next board
+                    return
+
+            # otherwise the forced board, doesn't have a result and the new move
+            # must be played on it
+            self.pos[self.nextBoard].make_move(move, self.playerJustMoved)
+            self.history.append(Move(board_idx=self.nextBoard, move_idx=move))
+            self.nextBoard = move
+            return
+
+        # if board is not None but nextBoard is None (i.e. start of game)
+        self.pos[board].make_move(move, self.playerJustMoved)
+        self.history.append(Move(board_idx=board, move_idx=move))
+        self.nextBoard = move
 
     def take_move(self):
-        pass
+        move = self.history.pop()
+        self.pos[move.board_idx].take_move(move.move_idx)
+        # todo need to update self.nextBoard
+
+    def get_all_moves(self) -> List[Tuple[int, int]]:
+        """Get all possible moves in the form of a list of tuples
+        containing (Board index, Move index).
+        """
+        all_moves = []
+        for idx, board in enumerate(self.pos):
+            if board.get_result() is not None:
+                continue
+
+            moves = board.get_moves()
+            if moves:
+                all_moves.extend(zip([idx]*len(moves), moves))
+        return all_moves
 
     def get_moves(self):
-        pass
+        """If a board is forced, return only the moves from that board.
+        If a board is forced but that board already has a result,
+        return all possible moves from all the other boards. If no
+        board is forced, return all possible moves.
+        """
+        # at the start of the game return all possible moves
+        if self.nextBoard is None:
+            return self.get_all_moves()
 
-    def get_result(self, player_jm):
-        pass
+        # if a forced board has a result -> return list of moves for
+        # all other boards that do not yet have a result
+        if self.pos[self.nextBoard].get_result() is not None:
+            all_moves = []
+            for idx, board in enumerate(self.pos):
+                if idx != self.nextBoard and board.get_result() is None:
+                    moves = board.get_moves()
+                    if moves:
+                        all_moves.extend(zip([idx] * len(moves), moves))
+            return all_moves
+
+        # if forced board doesn't have a result yet => get all possible moves
+        # from that board
+        all_moves = []
+        moves = self.pos[self.nextBoard].get_moves()
+        if moves:
+            all_moves.extend(zip([self.nextBoard] * len(moves), moves))
+
+        return all_moves
+
+    def get_result(self, player_jm):  # todo use player_jm instead of self.
+        # build a list containing the results from each individual board
+        # where there is no result i.e. None => replace with 0
+        result_board = [board.get_result() for board in self.pos]
+        result_board = [0 if result is None else result for result in result_board]
+
+        # find the result based on the result board
+        # (use an arbitrary instance of Board() to compute the result)
+        result = self.pos[0].get_result(board=result_board)
+
+        if result not in (self.playerJustMoved, -self.playerJustMoved) and not self.get_all_moves():
+            # if there is no winner and no available moves => DRAW
+            return DRAW
+        elif result in (self.playerJustMoved, -self.playerJustMoved):
+            return WIN if result == self.playerJustMoved else LOSS
+
+
+if __name__ == '__main__':
+    ub = UltimateBoard()
+    print(ub)
+    print('\n\n')
+    player = -ub.playerJustMoved
+    while ub.get_result(ub.playerJustMoved) is None:
+        moves = ub.get_moves()
+        board, move = random.choice(moves)
+        print(board, move)
+        ub.make_move(move, board)
+        print(ub)
+        print('\n\n')
+
+    print('Game finished. Result is', ub.get_result(player))
